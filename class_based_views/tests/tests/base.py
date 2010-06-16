2547e8465737e64eb9ed58307c3cb6da4841259b
Nick Fitzgerald <fitzgen@gmail.com>
2010-06-16 13:44:04 -0700

    Removing extraneous whitespace

--- class_based_views/tests/tests/base.py
+++ class_based_views/tests/tests/base.py
@@ -7,11 +7,11 @@ import unittest
 
 class AboutView(View):
     template_name = 'tests/about.html'
-    
+
 
 class PostAboutView(AboutView):
     allowed_methods = ['GET', 'POST']
-    
+
 
 class StrictAboutView(AboutView):
     strict_allowed_methods = True
@@ -20,31 +20,31 @@ class StrictAboutView(AboutView):
 class HashView(View):
     def get_content(self, request):
         return unicode(hash(self))
-    
+
 
 class JsonView(View):
     template_name = 'tests/apple_detail.html'
-    
+
     def __init__(self, *args, **kwargs):
         super(JsonView, self).__init__(*args, **kwargs)
         self.allowed_formats.append('json')
         self.format_mimetypes['json'] = 'application/json'
-    
+
     def render_json(self, request, *args, **kwargs):
         return simplejson.dumps(self.get_resource(request, *args, **kwargs))
-    
+
     def get_resource(self, request, color='red', **kwargs):
         return {'apple': {
             'color': color,
         }}
-    
+
 
 class ContextArgsJsonView(JsonView):
     def get_context(self, request, extra='', **kwargs):
         context = super(ContextArgsJsonView, self).get_context(request)
         context['extra'] = extra
         return context
-    
+
 
 class DefaultJsonView(JsonView):
     default_format = 'json'
@@ -55,21 +55,21 @@ class ContextJsonView(JsonView):
         context = super(ContextJsonView, self).get_context(request, *args, **kwargs)
         context['tasty'] = True
         return context
-    
+
 
 class ContextProcessorJsonView(JsonView):
     context_processors = [
         lambda r: {'tasty': True}
     ]
-    
+
 
 class ViewTest(unittest.TestCase):
     rf = RequestFactory()
-    
+
     def _assert_about(self, response):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, '<h1>About</h1>')
-    
+
     def test_get_only(self):
         """
         Test a view which only allows GET falls through to GET on other methods.
@@ -79,7 +79,7 @@ class ViewTest(unittest.TestCase):
         self._assert_about(AboutView(
             self.rf.get('/about/', REQUEST_METHOD='FAKE')
         ))
-    
+
     def test_get_and_post(self):
         """
         Test a view that simply renders a template and allows both GET and POST
@@ -89,7 +89,7 @@ class ViewTest(unittest.TestCase):
         self._assert_about(PostAboutView(
             self.rf.get('/about/', REQUEST_METHOD='FAKE')
         ))
-    
+
     def test_calling_more_than_once(self):
         """
         Test a view can only be called once.
@@ -97,7 +97,7 @@ class ViewTest(unittest.TestCase):
         request = self.rf.get('/about/')
         view = HashView
         self.assertNotEqual(view(request), view(request))
-    
+
     def test_strict_get_only(self):
         """
         Test a view which strictly only allows GET does not allow other methods.
@@ -111,12 +111,12 @@ class ViewTest(unittest.TestCase):
         )
         self.assertEqual(response.status_code, 405)
         self.assertEqual(response['Allow'], 'GET')
-    
+
     def _assert_html_apple(self, response):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response['Content-type'], 'text/html')
         self.assertEqual(response.content, 'This is a red apple')
-    
+
     def _assert_json_apple(self, response):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response['Content-type'], 'application/json')
@@ -124,7 +124,7 @@ class ViewTest(unittest.TestCase):
             simplejson.loads(response.content)['apple']['color'],
             'red'
         )
-    
+
     def test_json_default_html(self):
         """
         Test a view with different formats returns HTML by default.
@@ -151,7 +151,7 @@ class ViewTest(unittest.TestCase):
         self._assert_html_apple(DefaultJsonView(
             self.rf.get('/apple/?format=html')
         ))
-    
+
     def _assert_tasty(self, view):
         response = view(self.rf.get('/apple/'))
         self.assertEqual(response.content, 'This is a tasty red apple')
@@ -195,13 +195,13 @@ class ViewTest(unittest.TestCase):
 
 class DecoratorViewTest(TestCase):
     urls = 'class_based_views.tests.urls'
-    
+
     def test_decorators(self):
         """
-        Test any decorators applied with the ``decorators`` attribute are 
+        Test any decorators applied with the ``decorators`` attribute are
         applied.
         """
         res = self.client.get('/about/login-required/')
         self.assertEqual(res.status_code, 302)
         self.assertRedirects(res, 'http://testserver/accounts/login/?next=/about/login-required/')
-    
+
